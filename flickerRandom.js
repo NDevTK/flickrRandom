86b887f331a58b187b2fa671d70cb8fb25826707
Official Noob <31563761+OfficialNoob@users.noreply.github.com>
2019-05-18 15:21:04 +0100

    Update flickerRandom.js

--- flickerRandom.js
+++ flickerRandom.js
@@ -61,7 +61,7 @@ function FlickrImageApi(page) { // Run JSONP
 
 function shuffle(a) { // Shuffle array using seed
     for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(SessionRNG() * (i + 1));
+        const j = Math.floor(FlickrRND.SessionRNG() * (i + 1));
         [a[i], a[j]] = [a[j], a[i]];
     }
     return a;
